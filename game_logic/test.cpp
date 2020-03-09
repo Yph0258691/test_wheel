3beb218be48022c7088aa5d7ebee144669d77e2d
yinpinghua <986323930@qq.com>
2020-03-09 08:57:32 +0800

    新增 子文件生成lib

---
+++ game_logic/test.cpp
@@ -0,0 +1,14 @@
+#include "test.h"
+
+Test::Test()
+{
+
+}
+
+Test::~Test() {
+
+}
+
+void Test::display(){
+	std::cout << "开始工作" << std::endl;
+}
---
+++ game_logic/test.h
@@ -0,0 +1,13 @@
+#ifndef test_h__
+#define test_h__
+
+#include <iostream>
+
+class Test {
+public:
+	Test();
+	~Test();
+
+	void display();
+};
+#endif // test_h__
